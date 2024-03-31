07bf94ce70508c15047d0d8df0bbf7b0ea0b8e18
Marcos Lambir Torres <markos_laviana@yahoo.es>
2024-03-31 13:17:04 +0200

    feat: create folders with exercises and notes

---
+++ EJERCICIOS/ENUNCIADOS/README_EJC02.md
@@ -0,0 +1,7 @@
+Sitio Web para mostrar las obras de una galería de arte y poder comprarlas.
+
+Una página mosaico con todas las obras de arte donde se genere un carrito donde veo el listado de obras seleccionadas y el precio total para permitir a los usuarios ver las obras de una galería de arte y comprarlas.
+Aplicar CSS para el estilo, preferiblemente con flexbox o grid para la disposición de elementos.
+Mostrar las imágenes en forma de mosaico.
+El mosaico debe ser responsivo y minimalista y con aspecto visual elegante y atractivo.
+Implementar animaciones sutiles para las interacciones del usuario.
---
+++ EJERCICIOS/ENUNCIADOS/js/EJC02.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', function() {
+ const obras = [
+     { id: 1, nombre: "Obra 1", imagen: "https://via.placeholder.com/50x50/123", precio: 100 },
+     { id: 2, nombre: "Obra 2", imagen: "https://via.placeholder.com/50x50/321", precio: 200 },
+     { id: 3, nombre: "Obra 3", imagen: "https://via.placeholder.com/50x50/331", precio: 300 },
+     { id: 4, nombre: "Obra 4", imagen: "https://via.placeholder.com/50x50/121", precio: 400 },
+     { id: 5, nombre: "Obra 5", imagen: "https://via.placeholder.com/50x50/111", precio: 500 },
+     { id: 6, nombre: "Obra 6", imagen: "https://via.placeholder.com/50x50/761", precio: 600 },
+     { id: 7, nombre: "Obra 7", imagen: "https://via.placeholder.com/50x50/441", precio: 700 },
+ ];
+
+ //muestra por consola las obras
+console.log(obras);
+});
