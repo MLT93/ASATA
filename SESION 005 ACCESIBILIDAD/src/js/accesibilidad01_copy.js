ee1bb1729ef261ab51e4f81ec3a19da502086b31
Marcos Lambir Torres <markos_laviana@yahoo.es>
2024-04-08 11:02:54 +0200

    feat: create section, accesibilidad

---
+++ SESION 005 ACCESIBILIDAD/src/js/accesibilidad01_copy.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const divInfo_HTML = document.getElementById("divInfo");
+
+  divInfo_HTML.onclick = function () {
+    return alert("Contenido informativo extra.");
+  };
+});
