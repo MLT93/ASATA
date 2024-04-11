aebbd759d625a5f6cbd9a7fca3c5d064d46660fc
Marcos Lambir Torres <markos_laviana@yahoo.es>
2024-04-11 12:20:56 +0200

    feat: start exercise

---
+++ EXAMENES/marcos-lambir-torres_examen-final/tunombreyapellido/js/script.js
@@ -0,0 +1,87 @@
+document.addEventListener("DOMContentLoaded", function () {
+  // FORMULARIO
+
+  const buttonSubmit = document.getElementById("buttonSubmit");
+  const buttonShow = document.getElementById("buttonShow");
+  const comments = document.getElementById("comments");
+
+  buttonSubmit.onclick = function (e) {
+    e.preventDefault();
+
+    // VALIDACIONES LOG IN
+    try {
+      /* Validación Nombre */
+      const user = document.getElementById("userID");
+      if (!user.value) {
+        throw { msg: "El nombre de usuario es obligatorio", element: user };
+      }
+      /* Validación de Password */
+      const password = document.getElementById("passwordID");
+      if (
+        !password.value ||
+        password.value.match(
+          /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&]{8,}$/,
+        ) ||
+        Number(password.value) < 8
+      ) {
+        throw {
+          msg: "La contraseña debe tener minimo 8 caracteres",
+          element: password,
+        };
+      }
+
+      /* Restablezco valores */
+      const formInputs = Array.from(document.querySelectorAll("input"));
+      formInputs.forEach((e) => {
+        e.style.borderColor = "";
+        e.style.backgroundColor = "";
+        e.style.color = "";
+      });
+
+      if (password.value === "asdf" && user.value === "asdf") {
+        alert("El usuario y contraseña son correctos");
+        window.document.open("pag02.html");
+      }
+    } catch (error) {
+      console.error(error.msg);
+      error.element.style.borderColor = "red";
+      if (error.element.style.borderColor) {
+        error.element.style.color = "white";
+      }
+      error.element.style.backgroundColor = "tomato";
+      error.element.focus();
+    }
+  };
+
+  buttonShow.onclick = function (e) {
+    e.preventDefault();
+
+    // VALIDACIÓN TEXTAREA
+    try {
+      /* Validación TextArea */
+      const textarea = document.getElementById("textareaID");
+      if (!textarea.value) {
+        throw { msg: "Este campo no puede estar vacío", element: textarea };
+      }
+
+      comments.innerHTML = `<p>${comments.value}</p>`
+
+      /* Restablezco valores */
+      const formInputs = Array.from(document.querySelectorAll("input"));
+      formInputs.forEach((e) => {
+        e.style.borderColor = "";
+        e.style.backgroundColor = "";
+        e.style.color = "";
+      });
+
+    } catch (error) {
+      console.error(error.msg);
+      error.element.style.borderColor = "red";
+      if (error.element.style.borderColor) {
+        error.element.style.color = "white";
+      }
+      error.element.style.backgroundColor = "tomato";
+      error.element.focus();
+    }
+  };
+});
---
+++ exa/src/js/login.js
@@ -0,0 +1,72 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const buttonSubmit = document.getElementById("buttonSubmit");
+
+  buttonSubmit.onclick = function (e) {
+    e.preventDefault();
+
+    try {
+      // VALIDACIONES
+      /* Validación Nombre */
+      const name = document.getElementById("nameID");
+      if (!name.value) {
+        throw { msg: "El nombre es obligatorio", element: name };
+      }
+      /* Validación Apellido */
+      const surname = document.getElementById("lastNameID");
+      if (!surname.value) {
+        throw { msg: "El apellido es obligatorio", element: surname };
+      }
+      /* Validación Edad */
+      const age = document.getElementById("ageID");
+      const parsedAge = new Date(age.value).getFullYear();
+      const currentAge = new Date().getFullYear();
+      console.log(parsedAge);
+      if (parsedAge > currentAge) {
+        throw {
+          msg: "El correo electrónico es obligatorio y debe tener una fecha válida",
+          element: email,
+        };
+      }
+      /* Validación Email */
+      const email = document.getElementById("emailID");
+      if (!email.value.match(/^[\w-.]+@([\w-]+.)+[\w-]{2,4}$/)) {
+        throw {
+          msg: "El correo electrónico es obligatorio y debe tener un formato válido",
+          element: email,
+        };
+      }
+      /* Validación de Password */
+      const password = document.getElementById("passwordID");
+      if (
+        !password.value ||
+        password.value.match(/^(?=\w*\d)(?=\w*[A-Z])(?=\w*[a-z])\S{8,16}$/)
+      ) {
+        throw { msg: "La contraseña no es correcta", element: password };
+      }
+
+      /**
+       * ToDo: Revisar el código
+       */
+
+      // RESTABLECER EL FORMATO DE ERRORES
+      /* Al lanzar `throw` un error, el input quedará en rojo aunque le pongamos el valor requerido */
+      /* Para evitar eso, seleccionamos todos los `input` del formulario y modificamos ese comportamiento */
+      /* Usamos `forEach` porque `querySelectorAll` devuelve un array de elementos */
+      const formInputs = Array.from(document.querySelectorAll("input"));
+      formInputs.forEach((e) => {
+        e.style.borderColor = "";
+        e.style.backgroundColor = "";
+      });
+
+      alert("Formulario enviado con éxito!");
+    } catch (error) {
+      console.error(error.msg);
+      error.element.style.borderColor = "red";
+      if (error.element.style.borderColor) {
+        error.element.style.color = "white";
+      }
+      error.element.style.backgroundColor = "tomato";
+      error.element.focus();
+    }
+  };
+});
