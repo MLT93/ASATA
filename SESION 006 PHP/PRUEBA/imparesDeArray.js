ba16ab3c0ad48b218371cf3d591c152379137cd0
Marcos Lambir Torres <markos_laviana@yahoo.es>
2024-04-12 14:34:21 +0200

    docs: update

---
+++ SESION 006 PHP/PRUEBA/imparesDeArray.js
@@ -0,0 +1,4 @@
+let array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+const impares = array.filter((e) => e % 2 === 1);
+console.log(impares);
---
+++ SESION 006 PHP/TESTS/testBucleFor.js
@@ -0,0 +1,5 @@
+let value = 1;
+for (let i = 1; i < 5; i++) {
+  value += i;
+  console.log(value);
+}
