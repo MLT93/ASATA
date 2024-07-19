33227b7a56ce03b02653b104816f45f045d04bf9
Marcos Lambir Torres <markos_laviana@yahoo.es>
2024-07-19 19:14:17 +0200

    feat: update .gitignore and change some comments on Login.tsx

--- PROYECTO/client/src/components/12_Form/Login.tsx
+++ PROYECTO/client/src/components/12_Form/Login.tsx
@@ -2,7 +2,7 @@ import axios from "axios";
 import { ChangeEvent, useEffect, useRef, useState } from "react";
 
 /* 
-  1. CONECTAR PHP, REACT VITE Y MYSQL
+  1. CONECTAR REACT + VITE CON PHP Y MYSQL
   2. PRIMERA PARTE: https://www.youtube.com/watch?v=Gu-Fl1zIVbE
   3. SEGUNDA PARTE: https://www.youtube.com/watch?v=4B2XJHFaFIc
   4. MOVER BUILD FILES AL SERVIDOR PHP: https://www.techstackk.com/programming/reactjs/xampp-reactjs?pid=523
@@ -18,7 +18,6 @@ import { ChangeEvent, useEffect, useRef, useState } from "react";
  */
 
 const Login = (): JSX.Element => {
-  // useState, FormData y Fetch
   const [user, setUser] = useState({ email: "", password: "" });
 
   const handleInputOnChangeText = (evt: ChangeEvent<HTMLInputElement>) => {
@@ -46,40 +45,42 @@ const Login = (): JSX.Element => {
 
   // ************************ GET ***************************
 
-  // const URL_GET_USUARIOS = "/api/users/users.php";
+  const URL_GET_USUARIOS = "/api/users/users.php";
 
-  // const [data, setData] = useState<Response>();
-  // const [error, setError] = useState<Error>();
-  // const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [data, setData] = useState<Response>();
+  const [error, setError] = useState<Error>();
+  const [isLoading, setIsLoading] = useState<boolean>(false);
 
-  // useEffect(() => {
-  //   void (async (url: string): Promise<void> => {
-  //     try {
-  //       setIsLoading(true);
-  //       const response = await axios.get(url);
-  //       if (!response) {
-  //         throw new Error("Error en la petición");
-  //       }
-  //       const result = (await response.data) as Response;
-  //       setData(result);
-  //     } catch (error) {
-  //       if (error instanceof Error) {
-  //         console.error("Error obtenido:", error.message);
-  //         setError(error);
-  //       } else {
-  //         console.error("Error desconocido");
-  //       }
-  //     } finally {
-  //       setIsLoading(false);
-  //     }
-  //   })(URL_GET_USUARIOS);
-  // }, []);
+  useEffect(() => {
+    void (async (url: string): Promise<void> => {
+      try {
+        setIsLoading(true);
+        const response = await axios.get(url);
+        if (!response) {
+          throw new Error("Error en la petición");
+        }
+        const result = (await response.data) as Response;
+        setData(result);
+      } catch (error) {
+        if (error instanceof Error) {
+          console.error("Error obtenido:", error.message);
+          setError(error);
+        } else {
+          console.error("Error desconocido");
+        }
+      } finally {
+        setIsLoading(false);
+      }
+    })(URL_GET_USUARIOS);
+  }, []);
 
-  // console.log(data);
-  // error ? console.log(error) : null;
-  // isLoading ? console.log(isLoading) : null;
+  console.log(data);
+  error ? console.log(error) : null;
+  isLoading ? console.log(isLoading) : null;
 
   // *********************** POST ***************************
+  // ! AQUÍ ESTÁ EL PROBLEMA, NO ME DEVUELVE NINGUNA INFORMACIÓN
+  // ! NO SÉ SI ES A CAUSA DE LA LLAMADA O PORQUÉ
 
   const handleSubmitForm = (event: { preventDefault: () => void }) => {
     event.preventDefault();
