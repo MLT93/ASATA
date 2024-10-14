702c9538052b313cab0ee2aa611711b43061fdf4
Marcos Lambir Torres <markos_laviana@yahoo.es>
2024-10-14 15:54:38 +0200

    refactor: update ejercicio1 HTML with JS & CSS (if the input empty, disable button)

--- EXAMENES/03_marcos-lambir-torres_accesibilidad/js/ejercicio01.js
+++ EXAMENES/03_marcos-lambir-torres_accesibilidad/js/ejercicio01.js
@@ -1,7 +1,31 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const buttonSubmit_HTML = document.getElementById("buttonSubmit");
+const buttonSubmit_HTML = document.getElementById('buttonSubmit');
+const username_HTML = document.getElementById('nameID');
+const msg = document.getElementById('msg');
 
-  buttonSubmit_HTML.onclick = function () {
-    alert("Registrado con éxito.");
+// Event listener para el DOM
+window.document.addEventListener('DOMContentLoaded', () => {
+  console.log('BEFORE', username_HTML.value);
+  buttonSubmit_HTML.disabled = true;
+
+  // Event listener para change (validación final) tipo useRef()
+  username_HTML.onchange = function (event) {
+    if (event.target.value.trim() !== '' && isNaN(event.target.value)) {
+      buttonSubmit_HTML.disabled = false;
+
+      // Event listener para el botón (submit)
+      buttonSubmit_HTML.onclick = function () {
+        msg.style.visibility = 'visible';
+      };
+    }
+    console.log('AFTER', event.target.value);
+  };
+
+  // Event listener para input (respuesta inmediata a cambios) tipo useEffect()
+  // Se pone para fuera del onchange y al final, para que se ejecute siempre (para que quede siempre disabled)
+  username_HTML.oninput = function (e) {
+    if (e.target.value.trim() === '') {
+      buttonSubmit_HTML.disabled = true;
+    }
+    console.log('AFTER (change)', e.target.value);
   };
 });
