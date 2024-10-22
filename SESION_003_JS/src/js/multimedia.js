f3c1a7d17b3a610f01971b8bee2f89ce534e0a90
Marcos Lambir Torres <markos_laviana@yahoo.es>
2024-10-22 15:56:36 +0200

    refactor: format documents and add  forgot <body> tag

--- SESION_003_JS/src/js/multimedia.js
+++ SESION_003_JS/src/js/multimedia.js
@@ -1,39 +1,39 @@
-const buttonFade = document.getElementById("buttonFade");
+const buttonFade = document.getElementById('buttonFade');
 buttonFade.onclick = function () {
-  const element = document.getElementById("fade");
+  const element = document.getElementById('fade');
 
   /* `classList` manipula las clases que posea el elemento HTML */
   /* `add()` es un método de la función que manipula las clases */
   /* Posee más métodos, como `remove()`, `toggle()` y más */
-  element.classList.add("fade-out");
+  element.classList.add('fade-out');
 };
 
-const buttonZoom = document.getElementById("buttonZoom");
+const buttonZoom = document.getElementById('buttonZoom');
 buttonZoom.onclick = function () {
-  const element = document.getElementById("zoom");
-  element.classList.add("zoom");
+  const element = document.getElementById('zoom');
+  element.classList.add('zoom');
 };
 
-const buttonOscilar = document.getElementById("buttonOscilar");
+const buttonOscilar = document.getElementById('buttonOscilar');
 buttonOscilar.onclick = function () {
-  const element = document.getElementById("oscilar");
-  element.classList.add("oscilar");
+  const element = document.getElementById('oscilar');
+  element.classList.add('oscilar');
 };
 
-const buttonBigRotate = document.getElementById("buttonBigRotate");
+const buttonBigRotate = document.getElementById('buttonBigRotate');
 buttonBigRotate.onclick = function () {
-  const element = document.getElementById("agrandarYRotar");
-  element.classList.add("big-rotate");
+  const element = document.getElementById('agrandarYRotar');
+  element.classList.add('big-rotate');
 };
 
-const buttonReset = document.getElementById("buttonReset");
+const buttonReset = document.getElementById('buttonReset');
 buttonReset.onclick = function () {
   /* Tomo todos los elementos con clase `.box` */
   /* Itero el array que devuelve `querySelectorAll()` */
   /* Por cada elemento del array restablezco sus clases precedentes asignándole una única clase `box` */
   /* Esto permite restablecer todos los elementos HTML */
-  const boxes = document.querySelectorAll(".box");
-  boxes.forEach((e) => (e.className = "box"));
+  const boxes = document.querySelectorAll('.box');
+  boxes.forEach((e) => (e.className = 'box'));
 };
 
 // CANVAS
@@ -66,9 +66,9 @@ buttonReset.onclick = function () {
 const calcularRadianes = (grados) => (grados * Math.PI) / 180;
 
 /* Obtener el elemento donde voy a dibujar cualquier cosa */
-const lienzo = document.getElementById("lienzo");
+const lienzo = document.getElementById('lienzo');
 /* Tenemos que darle contexto al lienzo para que pueda pintar cosas */
-const ctx = lienzo.getContext("2d");
+const ctx = lienzo.getContext('2d');
 
 /* Dibujar la estructura del CÍRCULO en el contexto del lienzo */
 // 1º
@@ -76,7 +76,7 @@ const ctx = lienzo.getContext("2d");
 /* Si hay más dibujos en debajo de un mismo `beginPath()`, todos los elementos formarán parte de un mismo path. Es siempre mejor utilizar uno para cada elemento dibujado */
 ctx.beginPath();
 // 2º
-ctx.fillStyle = "rgb(70, 30, 23)";
+ctx.fillStyle = 'rgb(70, 30, 23)';
 // 3º
 ctx.arc(200, 100, 50, 0, 2 * Math.PI, true); // coordenadaX, coordenadaY, radio, startAngle, endAngle, antihorario
 // 4º
@@ -86,7 +86,7 @@ ctx.fill();
 // 1º
 ctx.beginPath();
 // 2º
-ctx.fillStyle = "rgba(0, 255, 0, 0.5)";
+ctx.fillStyle = 'rgba(0, 255, 0, 0.5)';
 // 3º
 ctx.rect(200, 70, 300, 150); // coordenadaX, coordenadaY, width, height
 // 4º
@@ -96,7 +96,7 @@ ctx.fill();
 // 1º
 ctx.beginPath();
 // 2º
-ctx.fillStyle = "rgba(230, 21, 21, 0.5)";
+ctx.fillStyle = 'rgba(230, 21, 21, 0.5)';
 // 3º
 ctx.ellipse(130, 100, 120, 50, 0, Math.PI / 2, (3 * Math.PI) / 2); // coordenadaX, coordenadaY, radioX, radioY, rotaciónEnRadianes, startAngle, endAngle
 // 4º
@@ -104,22 +104,22 @@ ctx.fill();
 
 /* Dibujar la estructura del RECTÁNGULO REDONDEADO en el contexto del lienzo */
 ctx.beginPath();
-ctx.strokeStyle = "red";
+ctx.strokeStyle = 'red';
 ctx.roundRect(170, 7, 150, 100, [10, 20, 0, 50]); // coordenadaX, coordenadaY, width, height, radio
 ctx.stroke();
 
 // LIENZO & CONTEXTO
-const lienzo2 = document.getElementById("lienzo-2");
-const ctx2 = lienzo2.getContext("2d");
+const lienzo2 = document.getElementById('lienzo-2');
+const ctx2 = lienzo2.getContext('2d');
 // CÍRCULO ENTERO DE REFERENCIA
 ctx2.beginPath();
-ctx2.fillStyle = "rgba(130, 130, 130, 0.75)";
+ctx2.fillStyle = 'rgba(130, 130, 130, 0.75)';
 ctx2.arc(250, 250, 100, 0, Math.PI * 2, false);
 ctx2.fill();
 // CÍRCULOS DEL EJERCICIO
 /* Primer círculo para los extremos superiores y segundo para el punto inferior */
 ctx2.beginPath();
-ctx2.fillStyle = "rgb(250, 220, 70)";
+ctx2.fillStyle = 'rgb(250, 220, 70)';
 let coordinateX = 250;
 let coordinateY = 250;
 let radius = 100;
@@ -141,7 +141,7 @@ ctx2.lineTo(250, 250);
 ctx2.fill();
 /* Segundo círculo independiente */
 ctx2.beginPath();
-ctx2.fillStyle = "rgb(190, 0, 0)";
+ctx2.fillStyle = 'rgb(190, 0, 0)';
 let coordX = 350;
 let coordY = 300;
 let radi = 30;
--- TEACHER_FOLDER/EXAMENES/EVALUACIONES HTML_CSS_JS/EXA UF1483/js/ejercicio02.js
+++ TEACHER_FOLDER/EXAMENES/EVALUACIONES HTML_CSS_JS/EXA UF1483/js/ejercicio02.js
@@ -1,52 +1,54 @@
 // Ajuste del tamaño del texto y el modo de alto contraste
 document.addEventListener('DOMContentLoaded', function() {
- const body = document.body;
- const increaseTextBtn = document.getElementById('increaseText');
- const decreaseTextBtn = document.getElementById('decreaseText');
- const highContrastBtn = document.getElementById('highContrast');
- const resetBtn = document.getElementById('reset');
-    // Establece un tamaño inicial de fuente si no está ya definido
-    if (!document.body.getAttribute('data-font-size')) {
-     let initialFontSize = 16; // Establece tu tamaño inicial deseado aquí
-     document.body.style.fontSize = initialFontSize + 'px';
-     document.body.setAttribute('data-font-size', initialFontSize.toString());
- }
+  const body = document.body;
+  const increaseTextBtn = document.getElementById('increaseText');
+  const decreaseTextBtn = document.getElementById('decreaseText');
+  const highContrastBtn = document.getElementById('highContrast');
+  const resetBtn = document.getElementById('reset');
+  // Establece un tamaño inicial de fuente si no está ya definido
+  if (!document.body.getAttribute('data-font-size')) {
+    let initialFontSize = 16; // Establece tu tamaño inicial deseado aquí
+    document.body.style.fontSize = initialFontSize + 'px';
+    document.body.setAttribute('data-font-size', initialFontSize.toString());
+  }
 
-
- // Función para cambiar el tamaño del texto
- function changeFontSize(change) {
+  // Función para cambiar el tamaño del texto
+  function changeFontSize(change) {
     // Suponiendo que el tamaño inicial de la fuente se ha establecido en algún lugar
     // en el atributo data-font-size del elemento body
-    let currentSize = parseFloat(document.body.getAttribute('data-font-size') || "16"); // "16" es un valor predeterminado
+    let currentSize = parseFloat(document.body.getAttribute('data-font-size') || '16'); // "16" es un valor predeterminado
 
     // Ajustar dentro de límites razonables para evitar que el texto sea demasiado grande o pequeño
     if (currentSize + change >= 12 && currentSize + change <= 24) {
-        document.body.style.fontSize = (currentSize + change) + 'px';
-        // Actualiza el atributo data-font-size con el nuevo tamaño para mantener la coherencia
-        document.body.setAttribute('data-font-size', (currentSize + change).toString());
+      document.body.style.fontSize = currentSize + change + 'px';
+      // Actualiza el atributo data-font-size con el nuevo tamaño para mantener la coherencia
+      document.body.setAttribute('data-font-size', (currentSize + change).toString());
     }
- }
+  }
 
- // Función para alternar el modo de alto contraste
- function toggleHighContrast() {
-     body.classList.toggle('high-contrast');
- }
+  // Función para alternar el modo de alto contraste
+  /* `classList` manipula las clases que posea el elemento HTML */
+  /* `add()` es un método de la función que manipula las clases */
+  /* Posee más métodos, como `remove()`, `toggle()` y más */
+  function toggleHighContrast() {
+    body.classList.toggle('high-contrast');
+  }
 
- // Función para restablecer los ajustes de accesibilidad a los valores predeterminados
- function resetAccessibilitySettings() {
-     body.style.fontSize = ''; // Restablecer el tamaño del texto al valor predeterminado
-     body.classList.remove('high-contrast'); // Desactivar el modo de alto contraste
- }
+  // Función para restablecer los ajustes de accesibilidad a los valores predeterminados
+  function resetAccessibilitySettings() {
+    body.style.fontSize = ''; // Restablecer el tamaño del texto al valor predeterminado
+    body.classList.remove('high-contrast'); // Desactivar el modo de alto contraste
+  }
 
- // Event listeners para los botones de control
- increaseTextBtn.addEventListener('click', function() {
-     changeFontSize(2); // Incrementar el tamaño del texto en 2px
- });
+  // Event listeners para los botones de control
+  increaseTextBtn.addEventListener('click', function () {
+    changeFontSize(2); // Incrementar el tamaño del texto en 2px
+  });
 
- decreaseTextBtn.addEventListener('click', function() {
-     changeFontSize(-2); // Disminuir el tamaño del texto en 2px
- });
+  decreaseTextBtn.addEventListener('click', function () {
+    changeFontSize(-2); // Disminuir el tamaño del texto en 2px
+  });
 
- highContrastBtn.addEventListener('click', toggleHighContrast);
- resetBtn.addEventListener('click', resetAccessibilitySettings);
+  highContrastBtn.addEventListener('click', toggleHighContrast);
+  resetBtn.addEventListener('click', resetAccessibilitySettings);
 });
